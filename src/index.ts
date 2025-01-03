8688e0cb64cd2f6b1b4d0914666de092d0b6e5dd
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2025-01-04 01:11:23 +1100

    Update incomingOnlyInternational regex to include additional patterns

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export const internationalService = /^00(?:1(?:[045689]|[37]\d)|9\d\d)$/;
-export const incomingOnlyInternational = /^10(?:3[1-3]|68)$/;
+export const incomingOnlyInternational = /^10(?:(?:3[1-3]|68)|10[034])$/;
 export const community = /^11(?:00|9\d)$/;
 export const interconnectionAndRouting = /^14[1-9]\d$/;
 export const dataNetworkAccess = /^019(?:2\d|8[4-8])$/
