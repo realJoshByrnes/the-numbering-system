3abe06b1aa254dc25342be97575761283bd1310f
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2025-01-04 01:32:58 +1100

    Update incomingOnlyInternational regex to include additional patterns

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 export const internationalService = /^00(?:1(?:[045689]|[37]\d)|9\d\d)$/;
-export const incomingOnlyInternational = /^10(?:(?:3[1-3]|68)|10[034])$/;
+export const incomingOnlyInternational = /^1(?:0(?:(?:3[1-3]|68)|10[034])|2014[1-3])$/;
 export const community = /^11(?:0(?:0|[1-9]\d{2})|9\d)$/;
 export const interconnectionAndRouting = /^14[1-9]\d$/;
 export const dataNetworkAccess = /^019(?:2\d|8[4-8])$/;
-export const operatorService = /^12(?:4(?:5\d|88)|55\d|68[689])$/;
+export const operatorService = /^12(?:4(?:5\d|88)|55\d|68[689])$/; // Needs additions from lines 119-150
 export const paging = /^0160[1-9]\d$/;
 export const virtualPrivateNetwork = /^188\d{2}$/;
 export const callingCard = /^189\d{2}$/;
+export const testingService = /^127220$/;
 
 const categories = {
     internationalService,
