ec23688e059cbc1ee4787cc1bf80b49d3fe66f06
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2025-01-04 01:17:36 +1100

    Add operatorService and paging regex patterns to categories

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ export const internationalService = /^00(?:1(?:[045689]|[37]\d)|9\d\d)$/;
 export const incomingOnlyInternational = /^10(?:(?:3[1-3]|68)|10[034])$/;
 export const community = /^11(?:00|9\d)$/;
 export const interconnectionAndRouting = /^14[1-9]\d$/;
-export const dataNetworkAccess = /^019(?:2\d|8[4-8])$/
+export const dataNetworkAccess = /^019(?:2\d|8[4-8])$/;
+export const operatorService = /^12(?:4(?:5\d|88)|55\d|68[689])$/;
+export const paging = /^0160[1-9]\d$/;
 
 const categories = {
     internationalService,
     incomingOnlyInternational,
     community,
     interconnectionAndRouting,
-    dataNetworkAccess
+    dataNetworkAccess,
+    operatorService,
+    paging,
 };
 
 export default categories;
