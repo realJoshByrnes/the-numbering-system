e9908ce8a1b33c8a0996a76a922185a2690d0564
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2025-01-04 01:28:48 +1100

    Update community regex
    Previous commit erroneously allowed 110\d without the trailing two digits.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const internationalService = /^00(?:1(?:[045689]|[37]\d)|9\d\d)$/;
 export const incomingOnlyInternational = /^10(?:(?:3[1-3]|68)|10[034])$/;
-export const community = /^11(?:0(?:0|[1-9]\d)|9\d)$/;
+export const community = /^11(?:0(?:0|[1-9]\d{2})|9\d)$/;
 export const interconnectionAndRouting = /^14[1-9]\d$/;
 export const dataNetworkAccess = /^019(?:2\d|8[4-8])$/;
 export const operatorService = /^12(?:4(?:5\d|88)|55\d|68[689])$/;
